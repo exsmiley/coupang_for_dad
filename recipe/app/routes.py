c312302420f2897a9d617214b2fc2a2f8f0a8265
GlennKim <glennkim@GlennKimui-noteubug.local>
2024-08-24 15:11:55 +0900

    [Feature] Add get recipe api]

--- recipe/app/routes.py
+++ recipe/app/routes.py
@@ -1,4 +1,7 @@
+# app/routes.py
+
 from flask import Blueprint, jsonify, request
+import requests
 
 api_blueprint = Blueprint('api', __name__)
 
@@ -10,3 +13,33 @@ def hello_world():
 def echo():
     data = request.json
     return jsonify(data)
+
+@api_blueprint.route('/get-recipes', methods=['GET'])
+def get_recipes():
+    url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch"
+    querystring = {
+        "query": "side salad",
+        "diet": "vegetarian",
+        "intolerances": "gluten",
+        "includeIngredients": "cheese,nuts",
+        "excludeIngredients": "eggs",
+        "instructionsRequired": "true",
+        "fillIngredients": "false",
+        "addRecipeInformation": "false",
+        "addRecipeInstructions": "false",
+        "addRecipeNutrition": "false",
+        "maxReadyTime": "45",
+        "ignorePantry": "true",
+        "sort": "max-used-ingredients",
+        "offset": "0",
+        "number": "10"
+    }
+
+    headers = {
+        "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        "x-rapidapi-key": "33ee2960efmshec60df803b3cbfap1f8843jsn5407f2ebb6ca"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    return jsonify(response.json())
